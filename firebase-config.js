b7060fb83a71a84ef56e0edce4dfc757a775c52a
infoj143-png <infoj.j143@gmail.com>
2026-06-22 13:41:30 +0500

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,7 @@
-// Firebase SDK Imports
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
-// Firebase Config
 const firebaseConfig = {
   apiKey: "AIzaSyDq7tm2GTmzxzOzJ8wMebR9GN81vwh5fh4",
   authDomain: "hafiz-watches.firebaseapp.com",
@@ -14,12 +12,9 @@ const firebaseConfig = {
   measurementId: "G-CN014NGHV7"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Services
-const auth = getAuth(app);
-const db = getFirestore(app);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
 
-// Export
-export { app, auth, db };
+export default app;
